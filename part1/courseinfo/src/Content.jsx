2dbded67659468200c005dba4163b60dc89ecfe5
ElenaCoder <97116051+ElenaCoder@users.noreply.github.com>
2025-02-13 20:17:53 +0200

    feat: add part1 courseinfo step3 implementation

---
+++ part1/courseinfo/src/Content.jsx
@@ -0,0 +1,16 @@
+import Part from "./Part"
+
+const Content = (props) => {
+    const { parts} = props;
+
+    return (
+      <div>
+        {parts.map((part, index) => (
+            <Part key={index} part={part.name} exercises={part.exercises} />
+      ))}
+      </div>
+    );
+  };
+
+
+export default Content;
