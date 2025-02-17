ed92edb383da3938849ecfd73f56a5ceb4d180f8
ElenaCoder <97116051+ElenaCoder@users.noreply.github.com>
2025-02-17 16:56:06 +0200

    feat: add part1 unicafe step2 implementation

--- part1/unicafe/src/Statistics.jsx
+++ part1/unicafe/src/Statistics.jsx
@@ -1,16 +1,24 @@
 const Statistics = ({ good, neutral, bad }) => {
-    if (good === 0 && neutral === 0 && bad === 0) {
-        return <div>No feedback given</div>;
+    const total = good + neutral + bad;
+
+    if (total === 0) {
+      return <div>No feedback given</div>;
     }
 
+    const average = (good - bad) / total;
+    const positive = (good / total) * 100;
+
     return (
-        <div>
-            <h2>statistics</h2>
-            <div>good {good}</div>
-            <div>neutral {neutral}</div>
-            <div>bad {bad}</div>
-        </div>
+      <div>
+        <h2>statistics</h2>
+        <div>good {good}</div>
+        <div>neutral {neutral}</div>
+        <div>bad {bad}</div>
+        <div>all {total}</div>
+        <div>average {average}</div>
+        <div>positive {positive} %</div>
+      </div>
     );
-};
+  };
 
 export default Statistics;
