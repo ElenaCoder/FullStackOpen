0a69ceb50f2f0060a6f6e4dd95c46e97fdc736fc
ElenaCoder <97116051+ElenaCoder@users.noreply.github.com>
2025-02-17 19:48:52 +0200

    feat: add part1 anecdotes step1 implementation

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -1,12 +1,31 @@
-function App() {
+import { useState } from 'react';
 
-  return (
-    <>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+const App = () => {
+    const [selected, setSelected] = useState(0);
 
-export default App
+    const anecdotes = [
+        'If it hurts, do it more often.',
+        'Adding manpower to a late software project makes it later!',
+        'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+        'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+        'Premature optimization is the root of all evil.',
+        'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+        'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+        'The only way to go fast, is to go well.',
+    ];
+
+    const getRandomIndex = () => Math.floor(Math.random() * anecdotes.length);
+
+    const getRandomAnecdote = () => {
+        setSelected(getRandomIndex());
+    };
+
+    return (
+        <div>
+            <button onClick={getRandomAnecdote}>next anecdotes</button>
+            <p>{anecdotes[selected]}</p>
+        </div>
+    );
+};
+
+export default App;
