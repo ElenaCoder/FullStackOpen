6bc718387a3d26aaeb66a9584085981c41ff7499
ElenaCoder <97116051+ElenaCoder@users.noreply.github.com>
2025-03-20 19:05:19 +0200

    feat: add part4 bloglist(back) step3 - 4.5

--- part4/bloglist/tests/list_helper.test.js
+++ part4/bloglist/tests/list_helper.test.js
@@ -2,6 +2,43 @@ const { test, describe } = require('node:test');
 const assert = require('node:assert');
 const listHelper = require('../utils/list_helper');
 
+// Test data: Empty list, one blog, multiple blogs
+const emptyList = [];
+
+const listWithOneBlog = [
+    {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+        likes: 5,
+    },
+];
+
+const multipleBlogs = [
+    {
+        _id: '1',
+        title: 'Blog 1',
+        author: 'Author A',
+        url: 'http://example.com/1',
+        likes: 2,
+    },
+    {
+        _id: '2',
+        title: 'Blog 2',
+        author: 'Author B',
+        url: 'http://example.com/2',
+        likes: 3,
+    },
+    {
+        _id: '3',
+        title: 'Blog 3',
+        author: 'Author C',
+        url: 'http://example.com/3',
+        likes: 7,
+    },
+];
+
 describe('dummy', () => {
     test('dummy returns one', () => {
         const blogs = [];
@@ -11,43 +48,8 @@ describe('dummy', () => {
 });
 
 describe('total likes', () => {
-    // Test data: Empty list, one blog, multiple blogs
-    const listWithOneBlog = [
-        {
-            _id: '5a422aa71b54a676234d17f8',
-            title: 'Go To Statement Considered Harmful',
-            author: 'Edsger W. Dijkstra',
-            url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
-            likes: 5,
-        },
-    ];
-
-    const multipleBlogs = [
-        {
-            _id: '1',
-            title: 'Blog 1',
-            author: 'Author A',
-            url: 'http://example.com/1',
-            likes: 2,
-        },
-        {
-            _id: '2',
-            title: 'Blog 2',
-            author: 'Author B',
-            url: 'http://example.com/2',
-            likes: 3,
-        },
-        {
-            _id: '3',
-            title: 'Blog 3',
-            author: 'Author C',
-            url: 'http://example.com/3',
-            likes: 7,
-        },
-    ];
-
     test('of empty list is zero', () => {
-        const result = listHelper.totalLikes([]);
+        const result = listHelper.totalLikes(emptyList);
         assert.strictEqual(result, 0);
     });
 
@@ -59,5 +61,30 @@ describe('total likes', () => {
     test('of a bigger list is calculated correctly', () => {
         const result = listHelper.totalLikes(multipleBlogs);
         assert.strictEqual(result, 12); // 2 + 3 + 7 = 12
+    });
+});
+
+describe('favorite blog', () => {
+    test('of empty list is null', () => {
+        const result = listHelper.favoriteBlog(emptyList);
+        assert.strictEqual(result, null);
+    });
+
+    test('when list has only one blog, return that blog', () => {
+        const result = listHelper.favoriteBlog(listWithOneBlog);
+        assert.deepStrictEqual(result, {
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            likes: 5,
+        });
+    });
+
+    test('of a bigger list is the one with most likes', () => {
+        const result = listHelper.favoriteBlog(multipleBlogs);
+        assert.deepStrictEqual(result, {
+            title: 'Blog 3',
+            author: 'Author C',
+            likes: 7,
+        });
     });
 });
--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,12 +1,30 @@
 const dummy = (blogs) => {
-    return 1
-  }
+    return 1;
+};
 
 const totalLikes = (blogs) => {
     return blogs.reduce((sum, blog) => sum + blog.likes, 0);
 };
-  
-  module.exports = {
+
+const favoriteBlog = (blogs) => {
+    if (blogs.length === 0) {
+        return null;
+    }
+
+    const favorite = blogs.reduce(
+        (max, blog) => (blog.likes > max.likes ? blog : max),
+        blogs[0],
+    );
+
+    return {
+        title: favorite.title,
+        author: favorite.author,
+        likes: favorite.likes,
+    };
+};
+
+module.exports = {
     dummy,
-    totalLikes
-  }
+    totalLikes,
+    favoriteBlog,
+};
