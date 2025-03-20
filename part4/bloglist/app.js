986e603d67bec42c062473b278d40b1fdff95848
ElenaCoder <97116051+ElenaCoder@users.noreply.github.com>
2025-03-20 15:54:55 +0200

    feat: add part4 bloglist(back) step2 - 4.2

---
+++ part4/bloglist/app.js
@@ -0,0 +1,33 @@
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
+const morgan = require('./utils/morganConfig')
+
+const blogsRouter = require('./controllers/blogs')
+
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+const app = express()
+
+mongoose.set('strictQuery', false)
+mongoose.connect(config.MONGODB_URI)
+  .then(() => logger.info('Connected to MongoDB'))
+  .catch(error => logger.error('Error connecting to MongoDB:', error.message))
+
+app.use(cors())
+app.use(express.json())
+app.use(morgan('tiny'))
+app.use(
+  morgan(
+    ':method :url :status :res[content-length] - :response-time ms :req-body',
+  ),
+)
+
+app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
