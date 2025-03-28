76eb8ac0ea80eac817e9f6da967129dc0df0e5ab
ElenaCoder <97116051+ElenaCoder@users.noreply.github.com>
2025-03-28 12:47:57 +0200

    feat: add user token-based auth-n to notes_backend

--- demos/notes_backend/controllers/notes.js
+++ demos/notes_backend/controllers/notes.js
@@ -1,6 +1,17 @@
 const notesRouter = require('express').Router()
+const jwt = require('jsonwebtoken')
+const config = require('../utils/config')
+
 const Note = require('../models/note')
 const User = require('../models/user')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
 
 notesRouter.get('/', async (request, response) => {
   const notes = await Note
@@ -23,7 +34,11 @@ notesRouter.put('/:id', async (request, response) => {
 notesRouter.post('/', async (request, response) => {
   const body = request.body
 
-  const user = await User.findById(body.userId)
+  const decodedToken = jwt.verify(getTokenFrom(request), config.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
 
   const note = new Note({
     content: body.content,
